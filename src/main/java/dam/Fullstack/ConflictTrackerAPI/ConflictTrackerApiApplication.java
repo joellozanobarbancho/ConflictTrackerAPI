9de1a9f033c48536f143444e8919fb8e1575541a
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2025-11-20 15:32:38 +0100

    first commit

---
+++ src/main/java/dam/Fullstack/ConflictTrackerAPI/ConflictTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package dam.Fullstack.ConflictTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConflictTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConflictTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dam/Fullstack/ConflictTrackerAPI/ConflictTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.Fullstack.ConflictTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConflictTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
