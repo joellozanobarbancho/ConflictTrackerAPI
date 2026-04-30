77f0f5592648b38c2f2243204a13bb02e2228104
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2026-04-30 16:40:15 +0200

    CorsConfig

--- src/main/java/dam/Fullstack/ConflictTrackerAPI/config/CorsConfig.java
+++ src/main/java/dam/Fullstack/ConflictTrackerAPI/config/CorsConfig.java
@@ -14,8 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://conflict-tracker-vue-indol.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins(
+                                "https://conflict-tracker-vue-nine.vercel.app",
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
